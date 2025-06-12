470b9569fc7f125306bc372a03974bb6e2d8b8ff
Tu Nguyen <nvt977@gmail.com>
2025-06-12 22:29:54 +0700

    add created products

---
+++ src/Services/Catalog/Catalog_API/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog_API.Models;
+
+namespace Catalog_API.Features.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductHandler : ICommandHanlder<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = request.Name,
+                Category = request.Category,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+
+            //throw new NotImplementedException();
+        }
+    }
+}
--- src/Services/Catalog/Catalog_API/Program.cs
+++ src/Services/Catalog/Catalog_API/Program.cs
@@ -1,8 +1,20 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    //config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+       .Where(a => !a.IsDynamic && !string.IsNullOrWhiteSpace(a.Location))
+       .ToArray();
+
+    config.RegisterServicesFromAssemblies(assemblies);
+});
 
 var app = builder.Build();
-
+app.MapCarter();
 // config the HTTP request pipline
 app.Run();
